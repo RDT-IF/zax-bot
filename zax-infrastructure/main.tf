ef281795a503f7d66417d78b95f24033c3eef77c
Iain E. Davis <iain@iaindavis.name>
2025-05-07 19:52:18 -0600

    Associate the droplet with a project

--- zax-infrastructure/main.tf
+++ zax-infrastructure/main.tf
@@ -6,13 +6,17 @@ resource "digitalocean_droplet" "zaxbot-1" {
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
-  
+
   connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
+    host    = self.ipv4_address
+    user    = "root"
+    type    = "ssh"
     private_key = file(var.pvt_key)
     timeout = "2m"
   }
 }
 
+resource "digitalocean_project" "zaxbot" {
+  name = "ZaxBot"
+  resources = [digitalocean_droplet.zaxbot-1.urn]
+}
