4b73bde64b6f14632ff053a85d98e55e46db2520
Iain E. Davis <iain@iaindavis.name>
2025-05-08 23:56:50 -0600

    Rescue the rose

--- src/main/java/org/rdtif/zaxbot/ZaxSlackBotModule.java
+++ src/main/java/org/rdtif/zaxbot/ZaxSlackBotModule.java
@@ -2,10 +2,14 @@ package org.rdtif.zaxbot;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import org.rdtif.zaxbot.interpreter.InterpreterModule;
 import org.rdtif.zaxbot.slack.SlackApplicationModule;
 import org.rdtif.zaxbot.userinterface.InputState;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ZaxSlackBotModule extends AbstractModule {
     @Override
@@ -24,5 +28,41 @@ public class ZaxSlackBotModule extends AbstractModule {
     @Singleton
     public InputState providesInputState() {
         return new InputState();
+    }
+
+    @Provides
+    @Named("Rose")
+    List<String> rose() {
+        return Arrays.asList(
+                "        .     .",
+                "    ...  :``..':",
+                "     : ````.'   :''::'",
+                "   ..:..  :     .'' :",
+                "``.    `:    .'     :",
+                "    :    :   :        :",
+                "     :   :   :         :",
+                "     :    :   :        :",
+                "      :    :   :..''''``::.",
+                "       : ...:..'     .''",
+                "       .'   .'  .::::'",
+                "      :..'''``:::::::",
+                "      '         `::::",
+                "                  `::.",
+                "                   `::",
+                "                    :::.",
+                "         ..:```.:'`. ::'`.",
+                "       ..'      `:.: ::",
+                "      .:        .:``:::",
+                "      .:    ..''     :::",
+                "       : .''         .::",
+                "        :          .'`::",
+                "                       ::",
+                "                       ::",
+                "                        :",
+                "                        :",
+                "                        :",
+                "                        :",
+                "                        ."
+        );
     }
 }
