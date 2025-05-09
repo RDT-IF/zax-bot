d82d52520d19ecfd0b877fd1a038776894fc4e04
Iain E. Davis <iain@iaindavis.name>
2025-05-08 20:39:18 -0600

    Remove unneeded infrastructure experiment

--- zax-bot-infrastructure/apply.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-terraform apply -var "do_token=${DO_PAT}" -var "pvt_key=$DO_KEY_FILE"
--- zax-bot-infrastructure/plan.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-terraform plan -var "do_token=${DO_PAT}" -var "pvt_key=$DO_KEY_FILE"
